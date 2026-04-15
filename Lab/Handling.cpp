2063ac33799104075a3c84d466d8dd20d36b415f
Vivek Landge <251070723@ycce.in>
2026-04-15 11:05:33 +0000

    Handling

---
+++ Lab/Handling.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // Open a file
+    ofstream file("GFG.txt");
+    
+    // Write the string to the file
+    file << "Welcome to GeeksforGeeks.";
+
+    return 0;
+}
